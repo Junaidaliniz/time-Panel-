01b3a9cd60d395aa914b1562ae1e9a6b26dc72ad
Junaidaliniz <157386475+Junaidaliniz@users.noreply.github.com>
2026-03-08 21:59:42 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,14 @@ const PORT = process.env.PORT || 3000;
 
 // --- IMPORT ALL PANELS ---
 const junaid = require("./api/junaid");
-const ayan = require("./api/ayan");   // Naya add kiya
-const ahmad = require("./api/ahmad"); // Naya add kiya
+const junaid = require("./api/ayan");
 
 // --- ROUTES ---
 app.use("/api/junaid", junaid);
-app.use("/api/ayan", ayan);     // Naya route set kiya
-app.use("/api/ahmad", ahmad);   // Naya route set kiya
+app.use("/api/ayan", ayan);
 
 // --- HEALTH CHECK ---
-app.get("/", (req, res) => res.send("API RUNNING ✅"));
+app.get("/", (req,res)=> res.send("API RUNNING ✅"));
 
 // --- START SERVER ---
-app.listen(PORT, "0.0.0.0", () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", ()=>console.log(`🚀 Server running on port ${PORT}`));
